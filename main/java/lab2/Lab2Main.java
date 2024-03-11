4a26d3e5e8305afd99d7ac7423f686f960985fa6
msz <marek.szymanski422@outlook.com>
2024-03-11 12:00:43 +0100

    lab2 touchup

--- main/java/lab2/Lab2Main.java
+++ main/java/lab2/Lab2Main.java
@@ -1,147 +1,47 @@
 package lab2;
 
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Lab2Main {
-    public static Random rng = new Random();
-
-    public void l2_main(String[] args) throws InterruptedException {
-        System.out.println("Hello world!");
-
-        SharedInput in = new SharedInput(10);
-        for (int i = 20; i < 32; i++) {
-            in.buffer.add(i);
-        }
+    public static void main(String[] args) throws InterruptedException {
+        SharedInput in = new SharedInput();
         SharedOutput out = new SharedOutput();
-        System.out.println("in.buffer: " + in.buffer.size());
 
-        ExecutorService executor = java.util.concurrent.Executors.newFixedThreadPool(4);
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 100; i++) {
+            in.put(i);
+        }
+
+
+        int threadNumber;
+        try {
+            threadNumber = Integer.parseInt(args[0]);
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid input, using default thread pool of 4");
+            threadNumber = 4;
+        }
+
+        System.out.println("Starting " + threadNumber + " threads");
+        ExecutorService executor = java.util.concurrent.Executors.newFixedThreadPool(threadNumber);
+        for (int i = 0; i < threadNumber; i++) {
             executor.execute(new MockThread(in, out));
         }
-//        for (int i = 0; i < 5; i++) {
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//            in.put(37);
-//        }
+
+
         Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextInt()) {
-            int a = scanner.nextInt();
-            if (a == 0) {
+        int scannedInt;
+        while(scanner.hasNextInt()) {
+            scannedInt = scanner.nextInt();
+            if (scannedInt == 0)
                 break;
-            }
-            in.put(a);
+            else
+                in.put(scannedInt);
         }
 
-
+        System.out.println("Shutting all threads down");
         executor.shutdownNow();
-        System.out.println("out.buffer: " + out.buffer.size());
-        System.out.println("in.buffer: " + out.primes.size());
-
-
-    }
-
-    private class SharedInput {
-        private ArrayList<Integer> buffer;
-        public SharedInput(int cap) {
-            this.buffer = new ArrayList<Integer>(cap);
-        }
-        public synchronized int take() throws InterruptedException{
-//            if (buffer.isEmpty()) {
-//                return 1;
-//            }
-            while (buffer.isEmpty()) {
-                wait();
-                System.out.println("Thred: " + Thread.currentThread().getName() + " is waiting");
-            }
-            notify();
-            return buffer.remove(0);
-        }
-        public synchronized void put(int num) {
-            buffer.add(num);
-            notify();
-        }
-    }
-
-    private class SharedOutput {
-        private ArrayList<Integer> buffer;
-        private ArrayList<Integer> primes;
-        public SharedOutput() {
-            this.buffer = new ArrayList<Integer>();
-            this.primes = new ArrayList<Integer>();
-        }
-        public synchronized void put(int num) {
-            buffer.add(num);
-        }
-        public synchronized void addPrime(int num) {
-            primes.add(num);
-        }
-
-    }
-
-    private class MockThread
-        implements Runnable {
-
-        private final SharedInput in;
-        private final SharedOutput out;
-
-        public MockThread(SharedInput in, SharedOutput out) {
-            this.in = in;
-            this.out = out;
-        }
-
-        private boolean isPrime(int num) {
-            if (num <= 1) {
-                return false;
-            }
-            for (int i = 2; i < num; i++) {
-                if (num % i == 0) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("Thred: " + Thread.currentThread().getName() + " is running");
-            int num;
-            for (int i = 0; i < 4; i++) {
-                if (Thread.currentThread().isInterrupted()) {
-                    System.out.println("Thred: " + Thread.currentThread().getName() + " is interrupted");
-                    break;
-                }
-//                synchronized (in) {
-//                    if (in.buffer.isEmpty()) {
-//                        try {
-//                            //in.wait();
-//                            System.out.println("Thred: " + Thread.currentThread().getName() + " is waiting");
-//                        } catch (InterruptedException e) {
-//                            throw new RuntimeException(e);
-//                        }
-//                    }
-//                }
-                try {
-                    num = in.take();
-                    System.out.println("Thred: " + Thread.currentThread().getName() + " is on num: " + num);
-
-                    if (isPrime(num))
-                        out.addPrime(num);
-                    else
-                        out.put(num);
-                }
-                catch (InterruptedException e) {
-                    System.out.println("Thred: " + Thread.currentThread().getName() + " is interrupted");
-                    break;
-                }
-            }
-            System.out.println("Thred: " + Thread.currentThread().getName() + " is done");
-        }
+        System.out.println("Input = " + in.buffer);
+        System.out.println("Output = " + out.buffer);
     }
 }
--- main/java/lab2/MockThread.java
+++ main/java/lab2/MockThread.java
@@ -6,6 +6,7 @@ public class MockThread implements Runnable {
     private SharedInput in;
     private SharedOutput out;
     private String threadName;
+    private Random rand = new Random();
 
     public MockThread(SharedInput _in, SharedOutput _out) {
         this.in = _in;
@@ -16,28 +17,52 @@ public class MockThread implements Runnable {
     public void run() {
         this.threadName = Thread.currentThread().getName();
         System.out.println(this.threadName + " started");
-        Random rng = new Random();
         int input;
-
 
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 input = in.take();
                 //System.out.println(this.threadName + " took {" + input + "} form SharedInput");
-                System.out.println(this.threadName + " started working on {" + input + "}");
-                Thread.sleep(rng.nextInt(5000 - 500) + 500);
-                System.out.println(this.threadName + " finished working on {" + input + "}");
-                out.put(input);
+                //System.out.println(this.threadName + " started working on {" + input + "}");
+                workPrime(input);
+                //System.out.println(this.threadName + " finished working on {" + input + "}");
             }
             catch (InterruptedException ie) {
-                System.out.println(this.threadName + " hit with InterruptException");
+                //System.out.println(this.threadName + " hit with InterruptException");
+                System.out.println(this.threadName + " interrupted");
                 break;
             }
         }
-        System.out.println(this.threadName + " interrupted");
-
 
         System.out.println(this.threadName  + " finishing");
+    }
+
+    private boolean checkPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void workPrime(int n) {
+        System.out.println(this.threadName + " working on {" + n + "}");
+        if (checkPrime(n)) {
+            out.put(n);
+        }
+    }
+
+    private void randSleep() {
+        try {
+            Thread.sleep(rand.nextInt(5000 - 500) + 500);
+        }
+        catch (InterruptedException ie) {
+            System.out.println(this.threadName + " hit with InterruptException while sleeping");
+        }
     }
 
 }
